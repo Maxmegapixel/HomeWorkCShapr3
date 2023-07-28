9212c82e69550c4f2a78960fb98f01f47584b811
Maksim <maxmegapixel@gmail.com>
2023-07-28 14:00:07 +0300

    задача 3

--- Program.cs
+++ Program.cs
@@ -1,17 +1,14 @@
-﻿// Задача 21
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("Введите координаты первой точки: ");
-int AX = int.Parse(Console.ReadLine()!);
-int AY = int.Parse(Console.ReadLine()!);
-int AZ = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine("Введите координаты второй точки: ");
-int BX = int.Parse(Console.ReadLine()!);
-int BY = int.Parse(Console.ReadLine()!);
-int BZ = int.Parse(Console.ReadLine()!);
-
-double AB = Math.Sqrt(Math.Pow(AX-BX,2) + Math.Pow(AY-BY, 2) + Math.Pow(AZ-BZ, 2));
-Console.WriteLine($"Расстояние: {AB:f2}");
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!);
+int I = 1;
+while (I <= Num) 
+{
+    
+    Console.WriteLine(Math.Pow(I, 3));
+    I++;
+}
